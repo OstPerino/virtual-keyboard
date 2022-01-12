fe1571badaac430198d840e5260503c4ca2d6a23
OstPerino <kucenko.nikita@gmail.com>
2022-01-13 02:39:33 +0700

    Write the function for controlling the shift

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const buttons   = document.querySelectorAll(".btn");
+const display   = document.querySelector(".display");
+const shift     = document.querySelector(".shift");
+const backspace = document.querySelector(".backspace");
+const clear     = document.querySelector(".clear");
+
+
+function shiftController(shift, buttons) {
+    shift.addEventListener("click", () => {
+
+        if (!shift.classList.contains("clicked")) {
+            shift.classList.add("clicked");
+
+            for (let button of buttons) {
+                
+                if (button == shift || button == backspace || button == clear) 
+                    continue;
+                
+                button.textContent = button.textContent.toUpperCase();
+            }
+
+        } else {
+            shift.classList.remove("clicked");
+
+            for (let button of buttons) {
+
+                if (button == shift || button == backspace || button == clear) 
+                    continue;
+
+                button.textContent = button.textContent.toLowerCase();
+            }
+            
+        }
+    });
+}
+
+shiftController(shift, buttons);
